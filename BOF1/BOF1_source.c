3eed8d65523d4a484208f9ab23ca40cd007fe184
ErelNahum <erelnahum2003@gmail.com>
2022-05-11 11:36:29 +0300

    added BOF1 challenge

---
+++ BOF1/BOF1_source.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char a[5];
+    printf("enter b: ");
+    char x[1];
+    char b[5];
+    scanf("%s", b);
+    if (!strcmp(a, "YEAH"))
+    {
+        printf("Good Job! You completed BOF1 challenge!\n");
+    }
+    else
+    {
+        printf("Failed. Try again. x = %c\n", x[0]);
+    }
+}
