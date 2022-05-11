d3c9d4fcb06588bbe645b02494dcf73dfe9a8c2f
ErelNahum <erelnahum2003@gmail.com>
2022-05-11 11:24:37 +0300

    added RE2 challenge

--- RE2/RE2_source.c
+++ RE2/RE2_source.c
@@ -1,4 +1,38 @@
-//
-// Created by T9134486 on 11/05/2022.
-//
+#include <stdio.h>
+#include <string.h>
+
+int validate_login(char *username, char *password)
+{
+    int result = 1;
+    for (int i = 0; i < strlen(username); i++)
+    {
+        if (username[i] + 3 != password[i])
+        {
+            result = 0;
+        }
+    }
+    return result;
+
+}
+
+int main()
+{
+    char username[100];
+    char password[100];
+    printf("Welcome to RE2 challenge!\n");
+    printf("Enter Username: ");
+    scanf("%100s", username);
+    printf("Enter Password: ");
+    scanf("%100s", password);
+    if (validate_login(username, password))
+    {
+        printf("Good Job! You completed RE2 challenge!\n");
+    }
+    else
+    {
+        printf("You're a noob. Ha Ha. Try again.\n");
+    }
+    return 0;
+
+}
 
