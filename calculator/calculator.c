5ff56bc95f036f77e6b83381462c6a0186f9fde4
Barr Kirel <barr.kirel.41@gmail.com>
2022-05-15 23:45:14 +0300

    Basic calculator code, zero testing if vurnable

---
+++ calculator/calculator.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LENGTH 1024
+#define XOR_KEY 0xAAAAAAAA
+#define ADD '+'
+#define SUB '-'
+
+
+void won_the_flag();
+int add(int x, int y);
+int sub(int x, int y);
+void update(char* buffer, int *x, int *y, char *op);
+
+int main(int argc, char** argv)
+{
+    char input[MAX_LENGTH] = { 0 };
+    int formulaCount = 0xAAAAAAAA; // We will have fun with XOR
+    int (*formula)(int, int) = NULL;
+    int x = 0, y = 0;
+    char op = '\0';
+
+    printf("Welcome to my calculator you can add and substract: \n");
+    do{
+        printf("Enter your formula (empty string to exit): ");
+        fgets(input, MAX_LENGTH, stdin);
+        if(strlen(input) > 0 && input[strlen(input) - 1] == '\n')
+        {
+            input[strlen(input) - 1] = 0;
+        }
+        formulaCount ^= XOR_KEY;
+        formulaCount++;
+        formulaCount ^= XOR_KEY;
+        if(strlen(input) > 0)
+        {
+            update(input, &x, &y, &op);
+            switch(op)
+            {
+                case ADD:
+                    formula = add;
+                    break;
+                case SUB:
+                    formula = sub;
+                    break;
+                default:
+                    printf("Invalid operation\n");
+                    break;
+            }
+            if(formula != NULL)
+            {
+                printf("Your %d is Result: %d\n", formulaCount ^ XOR_KEY, formula(x, y));
+            }
+        }
+        else
+            break;
+        
+    }while(1);
+    
+
+    return 0;
+}
+
+
+void won_the_flag()
+{
+    printf("You won the flag!\n");
+    exit(1);
+}
+
+int add(int x, int y)
+{
+    return x + y;
+}
+
+int sub(int x, int y)
+{
+    return x - y;
+}
+
+void update(char* buffer, int *x, int *y, char *op)
+{
+    char buff[16] = { 0 };
+    int i = 0;
+    // Finding the op
+    for(i = 0; buffer[i]; ++i)
+    {
+        if(buffer[i] == ADD || buffer[i] == SUB)
+        {
+            *op = buffer[i];
+            break;
+        }
+    }
+    // Finding the x
+    for(i = 0; buffer[i]; ++i)
+    {
+        if(buffer[i] == ADD || buffer[i] == SUB)
+        {
+            buff[i] = 0;
+            *x = atoi(buff);
+            break;
+        }
+        else
+            buff[i] = buffer[i];
+    }
+    // Finding the y
+    ++i;
+    for(int x = 0; buffer[i]; ++i, ++x)
+    {
+        buff[x] = buffer[i];
+    }
+    buff[i] = 0;
+    *y = atoi(buff);
+
+}
